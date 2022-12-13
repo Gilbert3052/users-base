b2e1279e026febfe900d4eae7a90b5312593aa2a
Gilbert <gilbertfernandez3052@gmail.com>
2022-12-13 19:47:51 -0400

    Update auth.router.js

--- src/auth/auth.router.js
+++ src/auth/auth.router.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 
 const postLogin = require('./auth.services')
 
-router.post('login', postLogin)
+router.post('/login', postLogin)
 
 module.exports = router  
